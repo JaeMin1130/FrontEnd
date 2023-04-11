03d50785b55d1ce9b57c190e7a851e4f05f3342d
JaeMin1130 <ija06598@gmail.com>
2023-04-11 14:02:06 +0900

    04/11

---
+++ FrontEndLesson/06/06.js
@@ -0,0 +1,36 @@
+
+document.addEventListener("DOMContentLoaded", () => {
+    const bts = document.querySelectorAll("button");
+    console.log(bts);
+    for (let i = 0; i < bts.length; i++) {
+        console.log(bts[i].textContent);
+        document.querySelector("#h2Id").textContent = bts[i].textContent;
+    }
+    // 버튼 안의 내용을 h2에 넣어라
+    // document.querySelector("#h2Id").textContent = bts.textContent;
+
+    // for문
+    // for (let i = 0; i < bts.length; i++) {
+    //     console.log(bts[i].textContent);
+    // }
+
+    // forEach문
+    // bts.forEach((item, i) => {
+    //     console.log(i, item.textContent);
+    // });
+
+    // for in문
+    // for (let i in bts) {
+    //     console.log(bts[i].textContent);
+    // }
+
+    // for of문
+//     for (const item of bts) {
+//         console.log(item.textContent)
+//     }
+//     for(const [i, item] of bts.entries()){
+//         console.log(i, item.textContent);
+//     }
+ }
+)
+;
---
+++ FrontEndLesson/06/Dice.js
@@ -0,0 +1,21 @@
+const show = () => {
+    event.preventDefault();
+    document.querySelector('#h2Id').innerHTML = randomDice(); 
+    
+    // 다른 방법(백틱, setAttribute)
+    // let n = Math.floor(Math.random() * diceArr.length);
+    // document.getElementById("h2Id").innerHTML = `<img src ="./diceImg/${n}.png">`;
+    // document.querySelector(".h2Class > img").setAttribute("src", `./diceImg/${n}.png`); 
+}
+function randomDice (){
+        let diceArr = ["<img src = './diceImg/1.png'>", 
+                        "<img src = './diceImg/2.png'>", 
+                        "<img src = './diceImg/3.png'>", 
+                        "<img src = './diceImg/4.png'>", 
+                        "<img src = './diceImg/5.png'>", 
+                        "<img src = './diceImg/6.png'>"] ;
+    
+        let randomDice = diceArr[Math.floor(Math.random() * diceArr.length)];
+    
+        return randomDice;
+    }
