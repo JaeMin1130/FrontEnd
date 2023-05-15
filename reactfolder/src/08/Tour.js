e2af40766ea5d50e61279f7fa6676dc1200afea9
JaeMin1130 <ija06598@gmail.com>
2023-05-15 17:07:23 +0900

    05/15

--- reactfolder/src/08/Tour.js
+++ reactfolder/src/08/Tour.js
@@ -5,47 +5,57 @@ import { useState, useEffect, useRef } from "react"
 const Tour = function () {
 
     const text = useRef()
-
+    const [pictureArr, setPictureArr] = useState([])
     useEffect(() => {
         text.current.focus()
     }, [])
 
-    const [pictureArr, setPictureArr] = useState([])
+    // fetch 함수
+    const fetchData = (url) => {
+        fetch(url)
+            .then((resp) => resp.json())
+            .then((data) => {
+                if (data && data.response && data.response.body && data.response.body.items && data.response.body.items.item) {
+                    let arr = data.response.body.items.item;
+                    setPictureArr(arr);
+                }else{
+                    text.current.value = "입력한 키워드에 대한 사진이 존재하지 않습니다."
+                }
+            })
+            .catch((err) => console.log(err))
+            }
 
+    // show 함수
     const show = function (e) {
         e.preventDefault()
         let encodedText = encodeURI(text.current.value)
         let url = `http://apis.data.go.kr/B551011/PhotoGalleryService1/gallerySearchList1?serviceKey=pwxPD7SvaQhz8AtCIjk2pem2kA4vkXCY5n4RIlREYOy1syfPerNWQQ09wWOQehCXOrObm74%2BO04%2BSTm04ksrzg%3D%3D&numOfRows=10&pageNo=1&MobileOS=ETC&MobileApp=AppTest&arrange=A&keyword=${encodedText}&_type=json`
-        fetch(url)
-            .then((resp) => resp.json())
-            .then((data) => {
-                let arr = data.response.body.items.item
-                setPictureArr(arr)
-            })
-            .catch((err) => console.log(err))
+        fetchData(url)
     }
 
+    // clear 함수
     const clear = (e) => {
         e.preventDefault()
+        setPictureArr([])
         text.current.value = ""
         text.current.focus()
     }
 
     return (
-        <main className="container">
-            <article>
+        <main className="container"> 
+            <article className={style.mainArticle}>
                 <header>
                     <h3>한국관광공사 관광사진 정보</h3>
                     <div className="grid">
                         <input ref={text} type="text" id="txt1" name="txt1" placeholder="키워드를 입력하세요." />
                         <div className="grid">
                             <button className="contrast" onClick={(e) => show(e)}>검색</button>
-                            <button className="contrast" onClick={(e) => clear(e)}>취소</button>
+                            <button className="contrast" onClick={(e) => clear(e)}>초기화</button>
                         </div>
                     </div>
                 </header>
             </article>
-            <Picture pictureArr={pictureArr} text = {text} setPictureArr = {setPictureArr} />
+            <Picture pictureArr={pictureArr} text={text} fetchData = {fetchData} />
         </main>
     )
 }
